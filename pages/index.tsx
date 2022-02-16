da65dec997ab07e129c820b5c0e12d8d83c83319
Elienai <elienay.soares07@gmail.com>
2022-02-15 21:49:50 -0300

    update(*): authentication server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,46 @@
 import { FormEvent, useState, useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import styles from "../styles/Home.module.css";
 import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
-  const [ email, setEmail ] = useState('')
-  const [ password, setPassword ] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { signIn } = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext);
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
     const data = {
       email,
       password,
-    }
-    await signIn(data)
+    };
+    await signIn(data);
   }
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Hello world!</h1>
+    <div>
+      <h1>Autenticação com Next.js</h1>
 
       <form onSubmit={handleSubmit}>
-        <input type="email" value={email} onChange={ e => setEmail(e.target.value)} />
-        <input type="password" value={password} onChange={ e => setPassword(e.target.value)} />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+        />
         <button type="submit">Entrar</button>
       </form>
     </div>
   );
-};
+}
 
 export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {},
   };
-})
+});
